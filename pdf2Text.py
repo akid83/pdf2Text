ce875289ab72ffb1b6592602cace31117976504b
CHUN-WEI CHEN <akid83@gmail.com>
2023-07-20 16:07:07 +0800

    Modify pdf2Text.py: Deal with PDF files only in THIS folder_path

--- pdf2Text.py
+++ pdf2Text.py
@@ -1,3 +1,4 @@
+import os
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -21,10 +22,16 @@ def extract_text_from_pdf(file_path):
     output_string.close()
     return text
 
-# 使用範例：
-pdf_file_path = "/Users/oscarchen/Documents/Fun/PDF_WORD compare/oscar_cv_2023.pdf"
-pdf_text = extract_text_from_pdf(pdf_file_path)
+# 取得當前的資料夾路徑
+folder_path = os.getcwd()
 
-output_file_path = "/Users/oscarchen/Documents/Fun/PDF_WORD compare/oscar_cv_2023.txt"
-with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    output_file.write(pdf_text)
+# 遍歷資料夾內的檔案
+for filename in os.listdir(folder_path):
+    if filename.endswith(".pdf"):
+        file_path = os.path.join(folder_path, filename)
+        pdf_text = extract_text_from_pdf(file_path)
+
+        # 建立對應的文字檔案
+        output_file_path = os.path.join(folder_path, f"{os.path.splitext(filename)[0]}.txt")
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            output_file.write(pdf_text)
